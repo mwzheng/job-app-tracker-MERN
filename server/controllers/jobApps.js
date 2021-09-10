6b1d620bf1c099fcb1d9b16043f4934d7ad5ad24
Mickey <mickey.zheng@yahoo.com>
2021-09-10 16:08:37 -0400

    Added POST request to add new job app

--- server/controllers/jobApps.js
+++ server/controllers/jobApps.js
@@ -20,7 +20,31 @@ exports.getJobApps = async (req, res) => {
 }
 
 exports.addJobApp = async (req, res) => {
+    try {
+        const newJobApp = await JobApp.create(req.body);
 
+        return res.status(201).json({
+            success: true,
+            mssg: 'Added new job app',
+            newJobApp: newJobApp
+        });
+    } catch (err) {
+        if (err.name === 'ValidationError') {
+            const messages = Object.values(err.errors).map(val => val.message);
+
+            return res.status(400).json({
+                success: false,
+                mssg: 'Failed to add new job app',
+                type: 'Bad Request',
+                error: messages,
+            });
+        } else {
+            return res.status(500).json({
+                success: false,
+                error: 'Server error',
+            });
+        }
+    }
 }
 
 exports.updateJobApp = async (req, res) => {
